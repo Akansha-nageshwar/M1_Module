9b562b3c2f3b92dd086f55f1211508f5a3ed519c
Akansha-nageshwar <87815555+Akansha-nageshwar@users.noreply.github.com>
2022-03-27 16:45:55 +0530

    Update Calender_operations.h

--- 3_Implementation/inc/Calender_operations.h
+++ 3_Implementation/inc/Calender_operations.h
@@ -1,1 +1,42 @@
-
+/**
+ * @file Calendar.h
+ * @author Akansha Nageshwar
+ * @brief displays Calendar 
+ * @version 0.1
+ * @date 2022-03-27
+ * 
+ * 
+ */
+#ifndef __Calendar_H__
+#define __Calendar_H__
+/**
+ * @brief Day
+ * 
+ * @param day 
+ * @param month 
+ * @param year 
+ * @return int 
+ */
+int daynum(int day, int month, int year);
+/**
+ * @brief Month
+ * 
+ * @param monthnum 
+ * @return char* 
+ */
+char* getmonthname(int monthnum);
+/**
+ * @brief Days 
+ * 
+ * @param monthnum 
+ * @param year 
+ * @return int 
+ */
+int numofdays(int monthnum, int year);
+/**
+ * @brief prints Calendar
+ * 
+ * @param year 
+ */
+void printCalendar(int year);
+#endif
